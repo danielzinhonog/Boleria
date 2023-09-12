f670ee5a35db9446ce56027d3882f7d46738b3bf
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2023-09-12 11:56:32 -0300

    Add files via upload

---
+++ pao/pao.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pao',
+  templateUrl: './pao.page.html',
+  styleUrls: ['./pao.page.scss'],
+})
+export class PaoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
